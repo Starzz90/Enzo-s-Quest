3f3d2cb092ff485c4b8e2265f8957dbb0eb9b478
Enzo Davidson Tjen <enzodtjen@gmail.com>
2026-02-25 03:12:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function Page() {
     window.location.href = "https://starzz90.github.io/More-Achivements-section/";
 }
-function Dark(){
-    
+const collapse = document.getElementsByClassName("collapsible")
+
+for(let i = 0; i < collapse.length; i++){
+    collapse[i].addEventListener("click", function(){
+        this.classList.toggle("activeColl")
+        let content = this.nextElementSibling
+
+        if(content.style.display === "block"){
+            content.style,display = ''        
+        }else{
+            content.style.display = "block"
+        }
+    })
 }
