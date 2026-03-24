99fd32ebbdc72aa48cc801605a2a7a8b22b411a0
Starzz90 <enzodtjen@gmail.com>
2026-03-24 04:35:08 -0500

    STILL TESTING

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-function Page() {
-    window.location.href = "https://starzz90.github.io/More-Achivements-section/";
-}
-const collapse = document.getElementsByClassName("collapsible")
-
-for(let i = 0; i < collapse.length; i++){
-    collapse[i].addEventListener("click", function(){
-        this.classList.toggle("activeColl")
-        let content = this.nextElementSibling
-
-        if(content.style.display === "block"){
-            content.style,display = ''        
-        }else{
-            content.style.display = "block"
-        }
-    })
+function Page(){
+    window.location.href = "index (12).html";
 }
