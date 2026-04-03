d411271ec35409a2890ebc97dd9b5c59c78b2c8c
Starzz90 <enzodtjen@gmail.com>
2026-04-02 22:52:05 -0500

    changing and adding movement to bars

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function Page(){
     window.location.href = "https://starzz90.github.io/More-Achivements-section/";
 }
+const bars = document.querySelectorAll('.stats-fill');
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            const bar = entry.target;
+            const width = bar.getAttribute('data-width');
+
+            setTimeout(() => {
+                bar.style.width = width + '%';;
+            }, 500);
+            observer.unobserve(bar);
+        }
+    });
+}, {threshold: 0.5});
+
+bars.forEach(bar =>
+    observer.observe(bar)
+);
